ea32c8d2a1b41f5b68a2b8cb84d9a421bf9a5ed6
Leandro Akira <leandro.akira@gmail.com>
2019-10-15 06:48:00 -0300

    Criando mais testes

--- src/test/java/br/com/sicredi/TDDNaPratica/CalcularNumeroTest.java
+++ src/test/java/br/com/sicredi/TDDNaPratica/CalcularNumeroTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class CalcularNumeroTest {
 
     @Test
-    public void deveSomarCartasABC(){
+    public void deveSomarCartasABC() {
         String cartasEscolhidas = "ABC";
         CalcularNumero calcularNumero = new CalcularNumero();
         int numeroCalculado = calcularNumero.soma(cartasEscolhidas);
@@ -16,11 +16,38 @@ public class CalcularNumeroTest {
     }
 
     @Test
-    public void deveSomarCartasABCDEF(){
+    public void deveSomarCartasABCDEF() {
         String cartasEscolhidas = "ABCDEF";
         CalcularNumero calcularNumero = new CalcularNumero();
         int numeroCalculado = calcularNumero.soma(cartasEscolhidas);
 
         assertEquals(63, numeroCalculado);
+    }
+
+    @Test
+    public void deveSomarCartasA() {
+        String cartasEscolhidas = "A";
+        CalcularNumero calcularNumero = new CalcularNumero();
+        int numeroCalculado = calcularNumero.soma(cartasEscolhidas);
+
+        assertEquals(1, numeroCalculado);
+    }
+
+    @Test
+    public void deveSomarCartasB() {
+        String cartasEscolhidas = "B";
+        CalcularNumero calcularNumero = new CalcularNumero();
+        int numeroCalculado = calcularNumero.soma(cartasEscolhidas);
+
+        assertEquals(2, numeroCalculado);
+    }
+
+    @Test
+    public void deveSomarCartasAB() {
+        String cartasEscolhidas = "AB";
+        CalcularNumero calcularNumero = new CalcularNumero();
+        int numeroCalculado = calcularNumero.soma(cartasEscolhidas);
+
+        assertEquals(3, numeroCalculado);
     }
 }
