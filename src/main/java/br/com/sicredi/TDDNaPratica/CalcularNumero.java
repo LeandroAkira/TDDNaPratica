7457a437ba5047000ea2e9a726de04c69541774b
Leandro Akira <leandro.akira@gmail.com>
2019-10-15 06:28:56 -0300

    Implementando regra para teste passar

--- src/main/java/br/com/sicredi/TDDNaPratica/CalcularNumero.java
+++ src/main/java/br/com/sicredi/TDDNaPratica/CalcularNumero.java
@@ -3,6 +3,18 @@ package br.com.sicredi.TDDNaPratica;
 public class CalcularNumero {
 
     public int soma( String cartas){
-        return 0;
+
+        int numeroCalculado = 0 ;
+        if(cartas.contains("A")){
+            numeroCalculado += 1;
+        }
+        if(cartas.contains("B")){
+            numeroCalculado += 2;
+        }
+        if(cartas.contains("C")){
+            numeroCalculado += 4;
+        }
+
+        return numeroCalculado;
     }
 }
