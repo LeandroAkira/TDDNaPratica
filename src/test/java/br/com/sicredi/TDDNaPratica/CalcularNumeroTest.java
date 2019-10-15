a125b1c484ce9dbb3d92ff48b14f69f77be21fac
Leandro Akira <leandro.akira@gmail.com>
2019-10-15 05:43:42 -0300

    Teste Falhando

--- src/test/java/br/com/sicredi/TDDNaPratica/CalcularNumeroTest.java
+++ src/test/java/br/com/sicredi/TDDNaPratica/CalcularNumeroTest.java
@@ -1,4 +1,18 @@
 package br.com.sicredi.TDDNaPratica;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalcularNumeroTest {
+
+    @Test
+    public void deveSomarCartasABC(){
+        String cartasEscolhidas = "ABC";
+        int numeroEsperado = 7 ;
+
+        int numeroCalculado = soma(cartasEscolhidas);
+
+        assertEquals(numeroEsperado, numeroCalculado);
+    }
 }
